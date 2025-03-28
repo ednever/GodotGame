29069e5e8fd38da04aa1d29bca1f602e351a985f
Edgar Neverovski <edgarneverovskij@gmail.com>
2025-03-28 19:04:08 +0200

    Project restructurated

--- scripts/key.gd
+++ scripts/key.gd
@@ -31,11 +31,6 @@ func pick_up() -> void:
 func get_status() -> String:
 	return "Ключ: " + key_name + ", Статус: " + ("Подобран" if is_picked_up else "На месте")
 
-## Сигнал при входе в область
-#func _on_area_2d_body_entered(body):
-	#if body.is_in_group("player"):  # Предполагаем, что игрок в группе "player"
-		#pick_up()
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("CharacterBody2D"):  # Предполагаем, что игрок в группе "player"
+		pick_up()
