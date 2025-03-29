277e782d65e6cc900b04b7501e5e02c30cf0e406
Shizokkk <fuutyman@gmail.com>
2025-03-29 11:22:57 +0200

    SAMQI ORIGINAL MAIN IZ VSEX

--- scripts/game_rules/timer.gd
+++
@@ -1,9 +0,0 @@
-extends Timer
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Timer.start() # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame
-	
